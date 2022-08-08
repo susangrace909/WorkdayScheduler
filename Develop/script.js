de100431048b4cc723699f3fc23da189f6b6346a
susangrace909 <102876452+susangrace909@users.noreply.github.com>
2022-08-08 17:49:48 -0400

    storage works, save icon added

--- Develop/script.js
+++ Develop/script.js
@@ -2,47 +2,92 @@
 var currentDay = moment().format("MMM Do YY");
 var momentDay;
 document.getElementById("momentDay").textContent = currentDay;
+var timeBlock = document.querySelector(".time-block");
+var emptyTextbox = ["", "", "", "", "", "", "", ""];
+var createTask;
+var savedTask = JSON.parse(localStorage.getItem("taskInfo"));
+var timeBlock = document.querySelector(".time-block");
 
 //past,present,future color change
 var currentHour = moment().hour();
 function relativeTime() {
+  const taskStorage = savedTask || emptyTextbox;
+  console.log(taskStorage);
   for (var i = 1; i < 10; i++) {
     var ii = i + 8;
+    $("#hour" + ii).val(taskStorage[i - 1]?.text);
     if (currentHour > ii) {
-      $("#hour" + i).addClass("past");
+      $("#hour" + ii).addClass("past");
     } else if (currentHour === ii) {
-      $("#hour" + i).addClass("present");
+      $("#hour" + ii).addClass("present");
     } else {
-      $("hour" + i).addClass("future");
+      $("#hour" + ii).addClass("future");
     }
   }
-  relativeTime();
 }
 
 console.log(currentHour);
 
 //save functionality
 function saveTasks() {
-  for (var i = 0; i < createTask.length; i++) {
-    var ii = i + 1;
-    var taskInfoEntry = document.querySelector("#hour" + ii).value;
-    console.log(taskInfoEntry);
-    if (taskInfoEntry) {
-      createTask[i] = taskInfoEntry;
-    } else {
-      createTask[i] = "";
-    }
-    console.log(taskInfo);
-  }
-  localStorage.setItem("taskInfo", JSON.stringify(taskInfo));
+  let taskInfoEntry = [
+    {
+      id: "hour9",
+      text: $("#hour9").val(),
+    },
+    {
+      id: "hour10",
+      text: $("#hour10").val(),
+    },
+    {
+      id: "hour11",
+      text: $("#hour11").val(),
+    },
+    {
+      id: "hour12",
+      text: $("#hour12").val(),
+    },
+    {
+      id: "hour13",
+      text: $("#hour13").val(),
+    },
+    {
+      id: "hour14",
+      text: $("#hour14").val(),
+    },
+    {
+      id: "hour15",
+      text: $("#hour15").val(),
+    },
+    {
+      id: "hour16",
+      text: $("#hour16").val(),
+    },
+  ];
+  //   for (var i = 0; i < createTask.length; i++) {
+  //     var ii = i + 1;
+  //     taskInfoEntry = $("#hour" + ii).val();
+  //     console.log(taskInfoEntry);
+  //     if (taskInfoEntry) {
+  //       createTask[i] = taskInfoEntry;
+  //     } else {
+  //       createTask[i] = "";
+  //     }
+  //     console.log(taskInfoEntry);
+  //   }
+  localStorage.setItem("taskInfo", JSON.stringify(taskInfoEntry));
+  window.location.reload();
 }
-var saveBtn = document.querySelector(".saveBtn");
-saveBtn.addEventListener("click", saveTasks);
+
+timeBlock.addEventListener("click", function (event) {
+  if (!event.target.classList.contains("saveBtn")) {
+    return;
+  }
+  saveTasks();
+  console.log("Save button?");
+});
 
 //text box input
-var emptyTextbox = ["", "", "", "", "", "", "", ""];
-var createTask;
-var savedTask = JSON.parse(localStorage.getItem("createTask"));
 if (savedTask) {
   createTask = savedTask;
 } else {
@@ -60,8 +105,7 @@ function getTasks() {
 }
 getTasks();
 
-var timeBlock = document.querySelector(".time-block");
-var inputs;
+relativeTime();
 
 // timeBlock.addEventListener("input", function (event) {
 //   inputs = { [event.target.id]: event.target.value };
